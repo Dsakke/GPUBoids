123b12dc5e1624095de961d60bc427d2262f3d13
sam vanden bosch <sammeke119@live.com>
2022-01-09 18:43:33 +0100

    Added a custom shader to render boids more efficiently

---
+++ Assets/Scripts/BoidShader.shader
@@ -0,0 +1,61 @@
+
+Shader "Instanced/InstancedSurfaceShader" {
+    Properties{
+        _Glossiness("Smoothness", Range(0,1)) = 0.5
+        _Metallic("Metallic", Range(0,1)) = 0.0
+    }
+        SubShader{
+            Tags { "RenderType" = "Opaque" }
+            LOD 200
+
+            CGPROGRAM
+            #pragma enable_d3d11_debug_symbols
+            // Physically based Standard lighting model
+            #pragma surface surf Standard addshadow fullforwardshadows
+            #pragma multi_compile_instancing
+            #pragma instancing_options procedural:setup
+
+            sampler2D _MainTex;
+
+            struct Input {
+                float2 uv_MainTex;
+            };
+
+        #ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+            struct Boid
+            {
+                float3 position;
+                float3 velocity;
+            };
+            StructuredBuffer<Boid> g_Boids;
+        #endif
+
+            void setup()
+            {
+            #ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+                float3 right = cross(float3(0, 1, 0), g_Boids[unity_InstanceID].velocity);
+                float3 up = cross(g_Boids[unity_InstanceID].velocity, right);
+                unity_ObjectToWorld._11_21_31_41 = float4(normalize(right), 0);
+                unity_ObjectToWorld._12_22_32_42 = float4(normalize(up), 0);
+                unity_ObjectToWorld._13_23_33_43 = float4(g_Boids[unity_InstanceID].velocity, 0);
+                unity_ObjectToWorld._14_24_34_44 = float4(g_Boids[unity_InstanceID].position.xyz, 1);
+
+                unity_WorldToObject = unity_ObjectToWorld;
+                unity_WorldToObject._14_24_34 *= -1;
+                unity_WorldToObject._11_22_33 = 1.0f / unity_WorldToObject._11_22_33;
+            #endif
+            }
+
+            half _Glossiness;
+            half _Metallic;
+
+            void surf(Input IN, inout SurfaceOutputStandard o) {
+                o.Albedo = float3(1, 1, 1);
+                o.Metallic = _Metallic;
+                o.Smoothness = _Glossiness;
+                o.Alpha = 1;
+            }
+            ENDCG
+        }
+            FallBack "Diffuse"
+}
--- Assets/Scripts/Flock.cs
+++ Assets/Scripts/Flock.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public struct Boid // this is a struct that should only contain data about boids that is needed by the compute shader
+public struct BoidData // this is a struct that should only contain data about boids that is needed by the compute shader
 {
-    public Boid(Vector3 startPos)
+    public BoidData(Vector3 startPos)
     {
         position = startPos;
         velocity = Vector3.zero;
@@ -16,16 +16,17 @@ public struct Boid // this is a struct that should only contain data about boids
 
 public class Flock : MonoBehaviour
 {
+    readonly private int m_ThreadGroupSize = 1024; // This needs to be hardcoded in the shader so you should make sure this is the same as "threadGroupsSize" in the compute shader
+
+
     [Header("General settings")]
     [SerializeField]
     private ComputeShader m_BoidCompute = null;
-    [SerializeField]
-    private GameObject m_BoidObject = null;
     [Header("Flock Settings")]
     [SerializeField]
     private int m_NrBoids = 100;
-    private ComputeBuffer m_Buffer;
-    private Boid[] m_BoidData;
+    private ComputeBuffer m_BoidBuffer;
+    private BoidData[] m_BoidData;
     [SerializeField]
     private float m_WorldSize = 1000.0f;
     [SerializeField]
@@ -34,63 +35,78 @@ public class Flock : MonoBehaviour
     [SerializeField]
     private float m_Acceleration = 10.0f;
     [SerializeField]
+    [Range(0.1f, 20.0f)]
     private float m_MaxSpeed = 50.0f;
     [SerializeField]
     [Range(0.0f, 1.0f)]
-    private float m_Cohesion = 0.5f;
+    private float m_Cohesion = 0.158f;
     [SerializeField]
     [Range(0.0f, 1.0f)]
-    private float m_Allignment = 0.5f;
+    private float m_Allignment = 0.492f;
     [SerializeField]
     [Range(0.0f, 1.0f)]
-    private float m_Seperation = 0.5f;
+    private float m_Seperation = 0.162f;
     private List<Transform> m_BoidTransforms = new List<Transform>();
+
+    // Instanced Draw Arguments
+    [Header("Graphics Settings")]
+    [SerializeField]
+    private Mesh m_BoidMesh = null;
+    [SerializeField]
+    private Material m_Material = null; // Material must be instanced for good performance
+    private uint[] m_Args = new uint[5] { 0, 0, 0, 0, 0 };
+    private ComputeBuffer m_BufferWithArgs = null;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        m_BoidData = new Boid[m_NrBoids];
+        m_BoidData = new BoidData[m_NrBoids];
         for (int i = 0; i < m_NrBoids; ++i)
         {
-            m_BoidData[i] = new Boid(new Vector3(Random.Range(-m_WorldSize, m_WorldSize), Random.Range(-m_WorldSize, m_WorldSize), Random.Range(-m_WorldSize, m_WorldSize)));
-            GameObject newBoid = Instantiate(m_BoidObject);
-            m_BoidTransforms.Add(newBoid.transform);
+            m_BoidData[i] = new BoidData(new Vector3(Random.Range(-m_WorldSize, m_WorldSize), Random.Range(-m_WorldSize, m_WorldSize), Random.Range(-m_WorldSize, m_WorldSize)));
+            m_BoidData[i].velocity = Vector3.forward;
         }
-        UpdateBoids();
+
         int posVecSize = sizeof(float) * 3;
         int velocityVecSize = sizeof(float) * 3;
         int boidSize = posVecSize + velocityVecSize;
-        m_Buffer = new ComputeBuffer(m_NrBoids, boidSize);
+        m_BoidBuffer = new ComputeBuffer(m_NrBoids, boidSize);
+        m_BoidBuffer.SetData(m_BoidData);
+
+        m_BufferWithArgs = new ComputeBuffer(1, sizeof(uint) * 5, ComputeBufferType.IndirectArguments);
+        m_Args[0] = (uint)m_BoidMesh.GetIndexCount(0);
+        m_Args[1] = (uint)m_NrBoids;
+        m_Args[2] = (uint)m_BoidMesh.GetIndexStart(0);
+        m_Args[3] = (uint)m_BoidMesh.GetBaseVertex(0);
+        m_BufferWithArgs.SetData(m_Args);
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        int posVecSize = sizeof(float) * 3;
-        int velocityVecSize = sizeof(float) * 3;
-        int boidSize = posVecSize + velocityVecSize;
-
         SetComputeShaderInput();
         int kernelId = m_BoidCompute.FindKernel("CSMain");
-        m_BoidCompute.Dispatch(kernelId, m_NrBoids / 100, 1, 1);
-        m_Buffer.GetData(m_BoidData);
-        UpdateBoids();
+        int threadGroups = Mathf.CeilToInt(m_NrBoids / m_ThreadGroupSize);
+        m_BoidCompute.Dispatch(kernelId, threadGroups + 1, 1, 1);
+        m_BoidBuffer.GetData(m_BoidData);
     }
 
-    void UpdateBoids()
+    private void Update()
     {
-        for(int i = 0; i < m_NrBoids; i++)
-        {
-            m_BoidTransforms[i].position = m_BoidData[i].position;
-            m_BoidTransforms[i].forward = m_BoidData[i].velocity;
-        }
+        // Render
+        Bounds bounds = new Bounds(Vector3.zero, new Vector3(m_WorldSize * 2, m_WorldSize * 2, m_WorldSize * 2));
+        m_Material.SetBuffer("g_Boids", m_BoidBuffer);
+        Graphics.DrawMeshInstancedIndirect(m_BoidMesh, 0, m_Material, bounds,m_BufferWithArgs);
     }
+
 
     void SetComputeShaderInput()
     {
-        m_Buffer.SetData(m_BoidData);
         int kernel = m_BoidCompute.FindKernel("CSMain");
-        m_BoidCompute.SetBuffer(kernel, "g_Boids", m_Buffer);
+        m_BoidCompute.SetBuffer(kernel, "g_Boids", m_BoidBuffer);
         m_BoidCompute.SetFloat("g_WorldSize", m_WorldSize);
         m_BoidCompute.SetFloat("g_NeighborhoodRad", m_NeighborhoodRadius);
         m_BoidCompute.SetFloat("g_Acceleration", m_Acceleration);
@@ -101,8 +117,9 @@ public class Flock : MonoBehaviour
         m_BoidCompute.SetFloat("g_Allignment", m_Allignment);
     }
 
-    private void ondestroy()
+    private void OnDestroy()
     {
-        m_Buffer.Dispose();
+        m_BoidBuffer.Dispose();
+        m_BufferWithArgs.Dispose();
     }
 }
